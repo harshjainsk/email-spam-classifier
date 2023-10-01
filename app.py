63c363f49366267e08e1abcd8b73a79d719aca52
harshjainsk <harshjainsk531@gmail.com>
2023-10-01 22:37:43 +0530

    added code for downloading stopwords and punctuations using nltk

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
+import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
+
+
 import streamlit as st
 import pickle
 import string
 from nltk.corpus import stopwords
-import nltk
 from nltk.stem.porter import PorterStemmer
 
 ps = PorterStemmer()
